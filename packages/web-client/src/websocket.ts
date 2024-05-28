7f88ac84415aae31473bf07f961c2815547fefb0
Luke Watson <luke@futureismeta.io>
2024-05-28 15:21:08 +0800

    -

---
+++ packages/web-client/src/websocket.ts
@@ -0,0 +1,52 @@
+class WebSocketClient {
+    private url: string;
+    private ws: WebSocket | null;
+
+    constructor(url: string) {
+        this.url = url;
+        this.ws = null;
+    }
+
+    connect(
+        onMessage: (data: string) => void,
+        onError: (error: Event) => void,
+        onOpen: () => void,
+        onClose: () => void
+    ) {
+        this.ws = new WebSocket(this.url);
+
+        this.ws.onopen = () => {
+            console.log("WebSocket connection established");
+            if (onOpen) onOpen();
+        };
+
+        this.ws.onmessage = (event: MessageEvent) => {
+            console.log("Received message:", event.data);
+            if (onMessage) onMessage(event.data);
+        };
+
+        this.ws.onerror = (error: Event) => {
+            console.error("WebSocket error:", error);
+            if (onError) onError(error);
+        };
+
+        this.ws.onclose = () => {
+            console.log("WebSocket connection closed");
+            if (onClose) onClose();
+        };
+    }
+
+    sendMessage(message: string) {
+        if (this.ws) {
+            this.ws.send(message);
+        }
+    }
+
+    disconnect() {
+        if (this.ws) {
+            this.ws.close();
+        }
+    }
+}
+
+export { WebSocketClient };
